7d09efc6b9b68df0c98d405c33f07338a1234084
vinculando_intellij <git@github.com:SandovalBrandon1027/vinculando_intellij.git>
2023-11-18 19:06:53 -0500

    Subiendo el archivo de fundamentos

---
+++ Fundamentos.js
@@ -0,0 +1,58 @@
+//Variables
+
+//Strings
+
+
+
+
+//Numeros
+const numeroIntentos = "4"
+const numeroPuntos = 40
+
+console.log(numeroPuntos,numeroIntentos);
+console.log(typeof(numeroPuntos), typeof(numeroIntentos));
+
+//Reliazamos algunos ejemplos
+console.log("suma:",numeroPuntos+20);
+console.log("resta:",numeroPuntos-20);
+console.log("multiplicación:",numeroPuntos*20);
+console.log("división:",numeroPuntos/20);
+console.log("módulo:",numeroPuntos%20);
+
+
+
+
+
+
+
+
+
+
+// Booleanos
+let saldoTarjetaUsuario = 1500
+let saldoTarjeta = 400
+let estadoCuenta
+let emailConfirmado=null
+
+
+saldoTarjetaUsuario += 100
+console.log(saldoTarjetaUsuario);
+
+saldoTarjetaUsuario="1000"
+console.log(saldoTarjeta == saldoTarjetaUsuario)
+
+console.log(saldoTarjeta === saldoTarjetaUsuario)
+console.log(estadoCuenta === emailConfirmado);
+
+console.log(saldoTarjeta + parseInt(saldoTarjetaUsuario));
+
+console.log(saldoTarjeta != saldoTarjetaUsuario);
+
+console.log(saldoTarjeta !== parseInt(saldoTarjetaUsuario));
+
+
+
+//Condicionales
+
+
+//Bucles
